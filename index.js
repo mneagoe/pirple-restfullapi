587f80b8659741b6b0b2ce6679ecc321c8b1f565
Mauro <mneagoe@agn.gov.ar>
2018-10-10 11:45:52 -0300

    video 16

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ const server = http.createServer(function (req, res) {
   // Get the HTTP Method
   let method = req.method.toLowerCase();
 
+  // Get the headers as an object
+  let headers = req.headers;
+
   // Send the response
   res.end('hola mundo\n');
 
   // Log the response path
-  console.log(`Request received on path: ${ trimmedPath } with method: ${ method } and with these query string parameters`, queryStringObject);
+  console.log('Request received with these headers: ', headers);
 
 });
 
